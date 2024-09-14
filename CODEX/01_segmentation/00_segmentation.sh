86908a6b97a8c876d507e703b06fd6c85b589b92
wuwenrui555 <wuwenruiwwr@outlook.com>
2024-09-14 02:43:14 -0400

    update shell for TMA segmentation

--- CODEX/01_segmentation/00_segmentation.sh
+++
@@ -1,3 +0,0 @@
-python \
-    /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/02_crop_tma.py \
-    /mnt/nfs/home/wenruiwu/projects/Sarcoma/data_test/data_input/20240611_LMS-TMA_Scan1/20240611_LMS-TMA_Scan1_parameter.json
---
+++ CODEX/01_segmentation/00_segmentation_tma.sh
@@ -0,0 +1,14 @@
+path_parameter=/mnt/nfs/home/wenruiwu/projects/Sarcoma/data_test/data_input/20240611_LMS-TMA_Scan1/20240611_LMS-TMA_Scan1_parameter.json
+path_objects=/mnt/nfs/home/wenruiwu/projects/Sarcoma/data_test/data_output/20240611_LMS-TMA_Scan1
+tag=test_20240914
+
+# Crop TMA
+python \
+    /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/02_crop_tma.py \
+    ${path_parameter}
+
+# Segmentation
+python /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/03_segment_mesmer_tma.py \
+    ${path_objects} \
+    ${path_parameter} \
+    ${tag}
