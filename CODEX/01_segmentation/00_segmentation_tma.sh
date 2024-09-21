8cba304c3a075f582d977c33abfaad79fce4440a
wuwenrui555 <wuwenruiwwr@outlook.com>
2024-09-21 01:53:40 -0400

    update: sh for TAM segmentation

--- CODEX/01_segmentation/00_segmentation_tma.sh
+++ CODEX/01_segmentation/00_segmentation_tma.sh
@@ -2,7 +2,6 @@
 
 # edit your parameter ^ #####
 path_parameter=/mnt/nfs/home/wenruiwu/projects/Sarcoma/data_test/data_input/20240611_LMS-TMA_Scan1/20240611_LMS-TMA_Scan1_parameter.json
-path_objects=/mnt/nfs/home/wenruiwu/projects/Sarcoma/data_test/data_output/20240611_LMS-TMA_Scan1
 tag=test_20240914
 # edit your parameter & #####
 
@@ -10,12 +9,10 @@ tag=test_20240914
 source /opt/miniconda3/bin/activate cellSeg
 
 # Crop TMA
-python \
-    /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/02_crop_tma.py \
+python /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/02_crop_tma.py \
     ${path_parameter}
 
 # Segmentation
-python /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/03_segment_mesmer_tma.py \
-    ${path_objects} \
+python /mnt/nfs/home/wenruiwu/projects/Tutorials/CODEX/01_segmentation/03_segment_mesmer.py \
     ${path_parameter} \
     ${tag}
